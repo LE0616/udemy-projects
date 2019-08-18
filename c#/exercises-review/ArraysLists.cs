48e31a0f460e1dd22f2046a8fca79690eb91f982
LE (Lee) <lm61613@gmail.com>
2019-08-18 12:13:25 -0500

    C# Review: Topic 3 Setup

--- c#/exercises-review/ArraysLists.cs
+++ c#/exercises-review/ArraysLists.cs
@@ -5,24 +5,33 @@ namespace exercises_review
 {
   public class Lists
   {
-    public void Ex1
+    public void Ex1()
     {
+      var names = new List<string>();
 
+      while (true)
+      {
+          Console.Write("Enter a name (or press ENTER to continue): ");
+          var input = Console.ReadLine();
+
+
+
+      }
     }
-    // public void Ex2
+    // public void Ex2()
     // {
 
     // }
-    // public void Ex3
+    // public void Ex3()
     // {
 
     // }
 
-    // public void Ex4
+    // public void Ex4()
     // {
 
     // }
-    // public void Ex5
+    // public void Ex5()
     // {
 
     // }
